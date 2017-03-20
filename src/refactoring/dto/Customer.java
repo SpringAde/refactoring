5499d78cddd19bf1fcd4e352d1780143340a1c85
inyoung <inyoung inyoung.estrella6y@naver.com>
2017-03-20 21:08:52 +0900

    메서드 이동 / 추출 완료

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -42,39 +42,7 @@ public class Customer {
 
 		sb.append(String.format("누적대여료 : %s%n 적립포인트 : %s%n", totalAmount,frequentRanterPoints));
 		return sb.toString();
-	}
-
-	private double amountFor(Rental aRantal) {	
-		//비디오물당 대여료 
-		//1.일반물(2일)2000원,일일초과당1500원,적립1 ==> (2일 3000원) 일일초과2000, 적립 1
-		//2.아동물(3일)1500원,일일초과당1500,적립1 ==> (2일 2000원) 일일초과1500, 적립 1
-		//3.최신물(1일)3000원, 일일초과 3000,적립1+1	==> (2일 4000원) 일일초과4000, 적립 1+1
-		//4.Html 출력형식을 요구.
-		double result = 0;
-		Movie movie = aRantal.getMovie();
-		int priceCode = movie.getPriceCode();
-		int daysRented = aRantal.getDaysRented();	//해당 비디오물의 대여 기간 
-
-		switch(priceCode){
-		case Movie.REGULAR:
-			result = 0000;
-			if(daysRented>2){
-				result+=(daysRented-2)*1500;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			result = daysRented*4000;
-			break;
-		case Movie.CHILDREN:
-			result = 1500;
-			if(daysRented>3){
-				result+=(daysRented-3)*1500;
-			}
-			break;
-		}
-		return result;
-	}
-
+	}	
 	
 	/******************************* 클라이언트 요구사항 *******************************/
 	public String htmlStatement(){
